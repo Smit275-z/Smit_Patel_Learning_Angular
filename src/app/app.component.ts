5cbaf0dae36e8d24b690b3dfe991608b00faaa50
smit <w0849357@myacc.ca>
2025-02-01 19:12:27 -0800

    Initialized product array in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Smit_Patel_Learning_Angular';
-
-  // New variables
-  userName: string = 'Smit Patel'; 
-  isLoggedIn: boolean = false;   
+  products: Product[] = [
+    { id: 1, name: 'Laptop', price: 1200, category: 'Electronics', description: 'Powerful gaming laptop' },
+    { id: 2, name: 'Headphones', price: 150, category: 'Electronics', description: 'Noise-canceling headphones' },
+    { id: 3, name: 'Coffee Maker', price: 80, category: 'Home Appliance' },
+    { id: 4, name: 'Smartphone', price: 900, category: 'Electronics', description: 'Latest model with 5G' },
+    { id: 5, name: 'Book: Angular Guide', price: 35, category: 'Books' },
+    { id: 6, name: 'Desk Chair', price: 250, category: 'Furniture', description: 'Ergonomic office chair' }
+  ];
 }
