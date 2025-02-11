370873eef68ebe435cb861ba96841d4de239d35e
smit <w0849357@myacc.ca>
2025-02-11 11:11:41 -0800

    Added ContentList and ContentListItem components

---
+++ src/app/content-list-item/content-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-list-item',
+  imports: [],
+  templateUrl: './content-list-item.component.html',
+  styleUrl: './content-list-item.component.css'
+})
+export class ContentListItemComponent {
+
+}
