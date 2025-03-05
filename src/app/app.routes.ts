ea8a3f9c615c83b64df060fc8ef937a2432238ec
smit <w0849357@myacc.ca>
2025-03-05 12:26:46 -0400

    Completed Steps 3,4 and 5.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ContentListComponent }, 
+  { path: 'products', component: ContentListComponent } 
+];
